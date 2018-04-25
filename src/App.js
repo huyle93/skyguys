bb6d5fc8c3d2a221a203ae0e0632754e24c04eb2
ab2010 <ab116893@gmail.com>
2018-04-25 14:01:07 -0400

    add sass

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import ReactDOM from 'react-dom';
 import './App.css';
 import { Button } from 'react-bootstrap'
+import registerServiceWorker from './registerServiceWorker'
+
+
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+        project: []
+
+    }
+  }
+componentDidMount(){
+  let url = "http://18.219.22.166/wp-json/wp/v2/pages/16"
+  console.log(url)
+  fetch(url)
+  .then(response => response.json())
+  .then(response => {
+    this.setState({
+      project: response
+
+    })
+  })
+
+
+
+}
+
   render() {
+    console.log('I was triggered during render')
+
     return (
-      <div>
-        <Button bsStyle='success'> hello </Button>
+      <div className="App">
+      <h1>test</h1>
       </div>
     );
   }
